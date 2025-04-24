14fd6de8e8b06bd0388062363233100308b30cdf
Magan Dhaniya <158152178+magan110@users.noreply.github.com>
2025-04-24 23:59:16 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,6 @@ import 'package:learning2/dsr_entry_screen/dsr_retailer_in_out.dart';
 import 'package:learning2/screens/splash_screen.dart';
 
 void main() {
-  runApp(const MaterialApp(home: SplashScreen()));
+  runApp(const MaterialApp(debugShowCheckedModeBanner:false,home: SplashScreen()));
 }
 
