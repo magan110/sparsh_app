dbc930df36d35af156c7a704ccd82289458d2eb6
MAGAN <magandhaniya@gmail.com>
2025-04-25 16:25:59 +0530

    9

---
+++ lib/dsr_entry_screen/phone_call_with_unregisterd_purchaser.dart
@@ -0,0 +1,382 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:learning2/dsr_entry_screen/phone_call_with_builder.dart';
+import 'package:learning2/dsr_entry_screen/phone_call_with_unregisterd_purchaser.dart';
+import 'package:learning2/dsr_entry_screen/work_from_home.dart';
+import 'Meeting_with_new_purchaser.dart';
+import 'Meetings_With_Contractor.dart';
+import 'any_other_activity.dart';
+import 'btl_activites.dart';
+import 'check_sampling_at_site.dart';
+import 'dsr_entry.dart';
+import 'dsr_retailer_in_out.dart';
+import 'internal_team_meeting.dart';
+import 'office_work.dart';
+import 'on_leave.dart';
+
+class PhoneCallWithUnregisterdPurchaser extends StatefulWidget {
+  const PhoneCallWithUnregisterdPurchaser({super.key});
+
+  @override
+  State<PhoneCallWithUnregisterdPurchaser> createState() => _PhoneCallWithUnregisterdPurchaserState();
+}
+
+class _PhoneCallWithUnregisterdPurchaserState extends State<PhoneCallWithUnregisterdPurchaser> {
+
+  String? _processItem = 'Select';
+  final List<String> _processdropdownItems = [
+    'Select',
+    'Add',
+    'Update',
+  ];
+
+  String? _activityItem = 'Phone call with Unregistered Purchasers';
+  final List<String> _activityDropDownItems = [
+    'Select',
+    'Personal Visit',
+    'Phone Call with Builder/Stockist',
+    'Meetings With Contractor / Stockist',
+    'Visit to Get / Check Sampling at Site',
+    'Meeting with New Purchaser(Trade Purchaser)/Retailer',
+    'BTL Activities',
+    'Internal Team Meetings / Review Meetings',
+    'Office Work',
+    'On Leave / Holiday / Off Day',
+    'Work From Home',
+    'Any Other Activity',
+    'Phone call with Unregistered Purchasers',
+  ];
+
+  final TextEditingController _dateController = TextEditingController();
+  final TextEditingController _reportDateController = TextEditingController(); // Added controller for Report Date
+  DateTime? _selectedDate;
+  DateTime? _selectedReportDate; // Added state variable for Report Date
+
+  @override
+  void dispose() {
+    _dateController.dispose();
+    _reportDateController.dispose(); // Dispose the new controller
+    super.dispose();
+  }
+
+  Future<void> _pickDate() async {
+    final now = DateTime.now();
+    final picked = await showDatePicker(
+      context: context,
+      initialDate: _selectedDate ?? now,
+      firstDate: DateTime(1900),
+      lastDate: DateTime(now.year + 5),
+    );
+    if (picked != null) {
+      setState(() {
+        _selectedDate = picked;
+        _dateController.text = DateFormat('yyyy-MM-dd').format(picked);
+      });
+    }
+  }
+
+  Future<void> _pickReportDate() async { // Function to pick report date
+    final now = DateTime.now();
+    final picked = await showDatePicker(
+      context: context,
+      initialDate: _selectedReportDate ?? now,
+      firstDate: DateTime(1900),
+      lastDate: DateTime(now.year + 5),
+    );
+    if (picked != null) {
+      setState(() {
+        _selectedReportDate = picked;
+        _reportDateController.text = DateFormat('yyyy-MM-dd').format(picked);
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => DsrEntry()),
+            );
+          },
+          icon: Icon(Icons.arrow_back,color: Colors.white,),
+        ),
+        title: const Text(
+          'DSR Entry',
+          style: TextStyle(color: Colors.white, fontSize: 30),
+        ),
+        backgroundColor: Colors.blue,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Form(
+          child: ListView(
+            children: [
+              MediaQuery(
+                data: MediaQuery.of(context).copyWith(textScaleFactor: 1.0),
+                child: const Text(
+                  'Instructions',
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.blue,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              //! Process Type
+              MediaQuery(
+                data: MediaQuery.of(context).copyWith(textScaleFactor: 1.0),
+                child: const Text(
+                  'Process type',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+                ),
+              ),
+              const SizedBox(height: 10),
+              Container(
+                height: 50,
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  border: Border.all(color: Colors.grey.shade400, width: 1),
+                  color: Colors.white,
+                ),
+                child: DropdownButton<String>(
+                  dropdownColor: Colors.white,
+                  isExpanded: true,
+                  underline: Container(),
+                  value: _processItem,
+                  onChanged: (newValue) {
+                    if (newValue != null) {
+                      setState(() => _processItem = newValue);
+                    }
+                  },
+                  items: _processdropdownItems
+                      .map(
+                        (value) => DropdownMenuItem(
+                      value: value,
+                      child: Text(value,
+                          style: const TextStyle(fontSize: 16)),
+                    ),
+                  )
+                      .toList(),
+                ),
+              ),
+              const SizedBox(height: 20),
+              // ! Activity type
+              MediaQuery(
+                data: MediaQuery.of(context).copyWith(textScaleFactor: 1.0),
+                child: const Text(
+                  'Activity Type',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+                ),
+              ),
+              const SizedBox(height: 10),
+              Container(
+                height: 50,
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  border: Border.all(color: Colors.grey.shade400, width: 1),
+                  color: Colors.white,
+                ),
+                child: DropdownButton<String>(
+                  isExpanded: true,
+                  underline: Container(),
+                  value: _activityItem,
+                  onChanged: (newValue) {
+                    if (newValue != null) {
+                      setState(() => _activityItem = newValue);
+
+                      // 🔹 Navigate on Personal Visit
+                      if (newValue == 'Personal Visit') {
+                        Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (_) => const DsrRetailerInOut(),
+                          ),
+                        );
+                      }
+                      // 🔹 Navigate on Phone Call with Builder/Stockist
+                      if (newValue == 'Phone Call with Builder/Stockist') {
+                        Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (_) => const PhoneCallWithBuilder(),
+                          ),
+                        );
+                      }
+
+                      // 🔹 Navigate on Meetings With Contractor / Stockist
+                      if (newValue == 'Meetings With Contractor / Stockist') {
+                        Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (_) => const MeetingsWithContractor(),
+                          ),
+                        );
+                      }
+
+                      // 🔹 Navigate on Visit to Get / Check Sampling at Site
+                      if (newValue == 'Visit to Get / Check Sampling at Site') {
+                        Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (_) => const CheckSamplingAtSite(),
+                          ),
+                        );
+                      }
+
+                      // 🔹 Navigate on Meeting with New Purchaser(Trade Purchaser)/Retailer
+                      if (newValue ==
+                          'Meeting with New Purchaser(Trade Purchaser)/Retailer') {
+                        Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (_) => const MeetingWithNewPurchaser(),
+                          ),
+                        );
+                      }
+
+                      // 🔹 Navigate on BTL Activities
+                      if (newValue == 'BTL Activities') {
+                        Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (_) => const BtlActivites(),
+                          ),
+                        );
+                      }
+
+                      // 🔹 Navigate on Internal Team Meetings / Review Meetings
+                      if (newValue == 'Internal Team Meetings / Review Meetings') {
+                        Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (_) => const InternalTeamMeeting(),
+                          ),
+                        );
+                      }
+
+                      // 🔹 Navigate on Office Work
+                      if (newValue == 'Office Work') {
+                        Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (_) => const OfficeWork(),
+                          ),
+                        );
+                      }
+
+                      // 🔹 Navigate on On Leave / Holiday / Off Day
+                      if (newValue == 'On Leave / Holiday / Off Day') {
+                        Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (_) => const OnLeave(),
+                          ),
+                        );
+                      }
+
+                      // 🔹 Navigate on Work From Home
+                      if (newValue == 'Work From Home') {
+                        Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (_) => const WorkFromHome(),
+                          ),
+                        );
+                      }
+
+                      // 🔹 Navigate on Any Other Activity
+                      if (newValue == 'Any Other Activity') {
+                        Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (_) => const AnyOtherActivity(),
+                          ),
+                        );
+                      }
+
+                      // 🔹 Navigate on Phone call with Unregistered Purchasers
+                      if (newValue == 'Phone call with Unregistered Purchasers') {
+                        Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (_) =>
+                            const PhoneCallWithUnregisterdPurchaser(),
+                          ),
+                        );
+                      }
+                    }
+                  },
+                  items: _activityDropDownItems
+                      .map((value) => DropdownMenuItem(
+                    value: value,
+                    child: Text(value,
+                        style: const TextStyle(fontSize: 16)),
+                  ))
+                      .toList(),
+                ),
+              ),
+              const SizedBox(height: 20),
+              // ! Submission Date
+              MediaQuery(
+                data: MediaQuery.of(context).copyWith(textScaleFactor: 1.0),
+                child: const Text(
+                  'Submission Date',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+                ),
+              ),
+              const SizedBox(height: 10),
+              TextFormField(
+                controller: _dateController,
+                readOnly: true,
+                decoration: InputDecoration(
+                  hintText: 'Select Date',
+                  suffixIcon: IconButton(
+                    icon: const Icon(Icons.calendar_today),
+                    onPressed: _pickDate,
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+                onTap: _pickDate,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please select a date';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 20),
+              // ! Report Date
+              MediaQuery(
+                data: MediaQuery.of(context).copyWith(textScaleFactor: 1.0),
+                child: const Text(
+                  'Report Date',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+                ),
+              ),
+              const SizedBox(height: 10),
+              TextFormField(
+                controller: _reportDateController, // Use the new controller
+                readOnly: true,
+                decoration: InputDecoration(
+                  hintText: 'Select Date',
+                  suffixIcon: IconButton(
+                    icon: const Icon(Icons.calendar_today),
+                    onPressed: _pickReportDate, // Use the new function
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+                onTap: _pickReportDate, // Use the new function
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please select a date';
+                  }
+                  return null;
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
